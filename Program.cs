f761ff260d4cf47ac4ee20ba448a3f665bc96a30
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-08 20:09:24 +0530

    main program to run three test cases

--- Program.cs
+++ Program.cs
@@ -5,14 +5,13 @@ namespace TestMaximum
         public static void Main(String[] args)
         {
             
-            testCases<float> test = new testCases<float>();
-            Console.WriteLine("Given Max Number at 1st Position from {5.5, 4.4, 3.73}return the Same Number:Test Case(1) ");
-           Console.WriteLine( test.maxi(5.5F, 4.4F, 3.73F));
-            Console.WriteLine("Given Max Number at 2nd Position from {55.8, 89.76, 73.66} return the Same Number:Test Case(2) ");
-            Console.WriteLine(test.maxi(55.8F, 89.76F, 73.66F));
-            Console.WriteLine("Given Max Number at 3rd Position from {101.56, 447.01, 833.09} return the Same Number:Test Case(3) ");
-            Console.WriteLine(test.maxi(101.56F, 447.01F, 833.09F));
-
+            testCases<string> test = new testCases<string>();
+            Console.WriteLine("Given Max Number at 1st Position from {Peach\",\"Apple\", \"Banana}return the Same Number:Test Case(1) ");
+           Console.WriteLine( test.maxi("Peach","Apple", "Banana"));
+            Console.WriteLine("Given Max Number at 2nd Position from {icon\", \"volume\", \"shape} return the Same Number:Test Case(2) ");
+            Console.WriteLine(test.maxi("icon", "volume", "shape"));
+            Console.WriteLine("Given Max Number at 3rd Position from {\"docter\", \"lawyer\", \"sofware.E\"} return the Same Number:Test Case(3) ");
+            Console.WriteLine(test.maxi("docter", "lawyer", "sofware.E"));
            
 
         }
