151134b3f283b870cc10ee1e9e061bc99fb97818
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-08 19:46:41 +0530

    returning maximum of three numbers

---
+++ IntegerMax.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaximum
+{
+    public class testCases <T> where T : IComparable
+    {
+        
+        public T maxi(T a,T b,T c)
+        {
+            if (a.CompareTo(b) == 1 && a.CompareTo(c) == 1)
+                return a;
+            if (b.CompareTo(a) == 1 && b.CompareTo(c) == 1)
+                return b;
+            if (c.CompareTo(a) == 1 && c.CompareTo(b) == 1)
+                return c;
+            return a;
+
+        }
+        
+    }
+   
+
+}
