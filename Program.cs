7bec5bc37315eb0f85822f1b218ac42264afd65c
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-08 21:33:18 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,22 +1,4 @@
-namespace TestMaximum
-{
-    class Program
-    {
-        public static void Main(String[] args)
-        {
-            
-            testCases<string> test = new testCases<string>();
-            Console.WriteLine("Given Max Number at 1st Position from {Peach\",\"Apple\", \"Banana}return the Same Number:Test Case(1) ");
-           Console.WriteLine( test.maxi("Peach","Apple", "Banana"));
-            Console.WriteLine("Given Max Number at 2nd Position from {icon\", \"volume\", \"shape} return the Same Number:Test Case(2) ");
-            Console.WriteLine(test.maxi("icon", "volume", "shape"));
-            Console.WriteLine("Given Max Number at 3rd Position from {\"docter\", \"lawyer\", \"sofware.E\"} return the Same Number:Test Case(3) ");
-            Console.WriteLine(test.maxi("docter", "lawyer", "sofware.E"));
-           
-
-        }
-    }
-}using System.IO;
+using System.IO;
 using System;
 
 namespace TestMaximum
@@ -31,34 +13,34 @@ namespace TestMaximum
             
             maxy<int> test1 = new maxy<int>();
             Console.WriteLine("Given Max Number at 1st Position from {5,4,3}return the Same Number:Test Case(1) ");
-            Console.WriteLine(test1.maxmathod(5, 4, 3));
+            test1.maxmathod(5, 4, 3);
             Console.WriteLine("Given Max Number at 2nd Position from {55,88,73} return the Same Number:Test Case(2) ");
-            Console.WriteLine(test1.maxmathod(55, 89, 73));
+            test1.maxmathod(55, 89, 73);
             Console.WriteLine("Given Max Number at 3rd Position from {101,447,833} return the Same Number:Test Case(3) ");
-            Console.WriteLine(test1.maxmathod(101, 447, 833));
+            test1.maxmathod(101, 447, 833);
 
             Console.WriteLine("---------------------------------------------------------------------------");
 
 
             maxy<float> test2 = new maxy<float>();
             Console.WriteLine("Given Max Number at 1st Position from {5.5, 4.4, 3.73}return the Same Number:Test Case(1) ");
-            Console.WriteLine(test2.maxmathod(5.5F, 4.4F, 3.73F));
+            test2.maxmathod(5.5F, 4.4F, 3.73F);
             Console.WriteLine("Given Max Number at 2nd Position from {55.8, 89.76, 73.66} return the Same Number:Test Case(2) ");
-            Console.WriteLine(test2.maxmathod(55.8F, 89.76F, 73.66F));
+            test2.maxmathod(55.8F, 89.76F, 73.66F);
             Console.WriteLine("Given Max Number at 3rd Position from {101.56, 447.01, 833.09} return the Same Number:Test Case(3) ");
-            Console.WriteLine(test2.maxmathod(101.56F, 447.01F, 833.09F));
+            test2.maxmathod(101.56F, 447.01F, 833.09F);
 
             Console.WriteLine("---------------------------------------------------------------------------");
             maxy<string> test = new maxy<string>();
             
             Console.WriteLine("Given Max Number at 1st Position from {Peach\",\"Apple\", \"Banana}return the Same Number:Test Case(1) ");
-            Console.WriteLine(test.maxmathod("Peach", "Apple", "Banana"));
+            test.maxmathod("Peach", "Apple", "Banana");
 
             Console.WriteLine("Given Max Number at 2nd Position from {icon\", \"volume\", \"shape} return the Same Number:Test Case(2) ");
-            Console.WriteLine(test.maxmathod("icon", "volume", "shape"));
+            test.maxmathod("icon", "volume", "shape");
 
             Console.WriteLine("Given Max Number at 3rd Position from {\"docter\", \"lawyer\", \"sofware.E\"} return the Same Number:Test Case(3) ");
-            Console.WriteLine(test.maxmathod("docter", "lawyer", "sofware.E"));
+            test.maxmathod("docter", "lawyer", "sofware.E");
 
 
         }
