e741ecedf294a657a8619233b8d94ac44bb477c9
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-08 19:59:26 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,13 +5,13 @@ namespace TestMaximum
         public static void Main(String[] args)
         {
             
-            testCases<int> test = new testCases<int>();
-            Console.WriteLine("Given Max Number at 1st Position from {5,4,3}return the Same Number:Test Case(1) ");
-           Console.WriteLine( test.maxi(5, 4, 3));
-            Console.WriteLine("Given Max Number at 2nd Position from {55,88,73} return the Same Number:Test Case(2) ");
-            Console.WriteLine(test.maxi(55, 89, 73));
-            Console.WriteLine("Given Max Number at 3rd Position from {101,447,833} return the Same Number:Test Case(3) ");
-            Console.WriteLine(test.maxi(101, 447, 833));
+            testCases<float> test = new testCases<float>();
+            Console.WriteLine("Given Max Number at 1st Position from {5.5, 4.4, 3.73}return the Same Number:Test Case(1) ");
+           Console.WriteLine( test.maxi(5.5F, 4.4F, 3.73F));
+            Console.WriteLine("Given Max Number at 2nd Position from {55.8, 89.76, 73.66} return the Same Number:Test Case(2) ");
+            Console.WriteLine(test.maxi(55.8F, 89.76F, 73.66F));
+            Console.WriteLine("Given Max Number at 3rd Position from {101.56, 447.01, 833.09} return the Same Number:Test Case(3) ");
+            Console.WriteLine(test.maxi(101.56F, 447.01F, 833.09F));
 
            
 
